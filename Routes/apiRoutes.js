0eb112dcc2cc99b269976bacac7fc01fdf6fe73d
Jmorg21 <Jmorg21@gmail.com>
2022-07-10 15:39:54 -0400

    debugging

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { createNewNotes } = require('../db/index');
-const newNotes  = require('../db.json');
+const { createNewNotes } = require('../Develop/db/notes');
+const newNotes  = require('../Develop/db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = newNotes ;	
@@ -16,3 +16,5 @@ router.post('/notes', (req, res) => {
     const notes = createNewNotes(req.body, newNotes);	
     res.json(notes);	    
 });
+
+module.exports = router;
