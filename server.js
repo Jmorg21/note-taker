777bd3af27e60a15f055106bbe28d7d6f68adc43
Jmorg21 <Jmorg21@gmail.com>
2022-07-10 14:14:51 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('docs'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
