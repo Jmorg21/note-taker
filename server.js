56fd986e15de18d3f476634f4066aa96ca325e37
Jmorg21 <Jmorg21@gmail.com>
2022-07-10 14:16:05 -0400

    server.js update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
